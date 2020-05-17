68fce0ddeea73cb592d58c33e099203c95645ef9
Issac Angulo <issac.angulo@gmail.com>
2020-05-17 11:54:36 -0700

    took out unwanted accidental text in script.js

--- assets/script.js
+++ assets/script.js
@@ -125,7 +125,7 @@ var generator = {
             i++;
           }
         break;
-        case 3:generating
+        case 3:
           if(this.spec_val) {
             buffer+=this.getRandSpec();
             i++;
